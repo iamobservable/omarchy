1d44e366504eb072b4a628d10eca34797f2f10a2
Observable Art <123117871+iamobservable@users.noreply.github.com>
2025-08-15 18:29:07 -0500

    Comment installation steps

--- install.sh
+++ install.sh
@@ -27,60 +27,60 @@ show_subtext() {
 }
 
 # Install prerequisites
-source $OMARCHY_INSTALL/preflight/guard.sh
-source $OMARCHY_INSTALL/preflight/aur.sh
-source $OMARCHY_INSTALL/preflight/presentation.sh
-source $OMARCHY_INSTALL/preflight/migrations.sh
+#source $OMARCHY_INSTALL/preflight/guard.sh
+#source $OMARCHY_INSTALL/preflight/aur.sh
+#source $OMARCHY_INSTALL/preflight/presentation.sh
+#source $OMARCHY_INSTALL/preflight/migrations.sh
 
 # Configuration
-show_logo beams 240
-show_subtext "Let's install Omarchy! [1/5]"
-source $OMARCHY_INSTALL/config/identification.sh
-source $OMARCHY_INSTALL/config/config.sh
-source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-source $OMARCHY_INSTALL/config/fix-fkeys.sh
-source $OMARCHY_INSTALL/config/network.sh
-source $OMARCHY_INSTALL/config/power.sh
-source $OMARCHY_INSTALL/config/timezones.sh
-source $OMARCHY_INSTALL/config/login.sh
-source $OMARCHY_INSTALL/config/nvidia.sh
+#show_logo beams 240
+#show_subtext "Let's install Omarchy! [1/5]"
+#source $OMARCHY_INSTALL/config/identification.sh
+#source $OMARCHY_INSTALL/config/config.sh
+#source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
+#source $OMARCHY_INSTALL/config/fix-fkeys.sh
+#source $OMARCHY_INSTALL/config/network.sh
+#source $OMARCHY_INSTALL/config/power.sh
+#source $OMARCHY_INSTALL/config/timezones.sh
+#source $OMARCHY_INSTALL/config/login.sh
+#source $OMARCHY_INSTALL/config/nvidia.sh
 
 # Development
-show_logo decrypt 920
-show_subtext "Installing terminal tools [2/5]"
-source $OMARCHY_INSTALL/development/terminal.sh
-source $OMARCHY_INSTALL/development/development.sh
-source $OMARCHY_INSTALL/development/nvim.sh
-source $OMARCHY_INSTALL/development/ruby.sh
-source $OMARCHY_INSTALL/development/docker.sh
-source $OMARCHY_INSTALL/development/firewall.sh
+#show_logo decrypt 920
+#show_subtext "Installing terminal tools [2/5]"
+#source $OMARCHY_INSTALL/development/terminal.sh
+#source $OMARCHY_INSTALL/development/development.sh
+#source $OMARCHY_INSTALL/development/nvim.sh
+#source $OMARCHY_INSTALL/development/ruby.sh
+#source $OMARCHY_INSTALL/development/docker.sh
+#source $OMARCHY_INSTALL/development/firewall.sh
 
 # Desktop
-show_logo slice 60
-show_subtext "Installing desktop tools [3/5]"
-source $OMARCHY_INSTALL/desktop/desktop.sh
-source $OMARCHY_INSTALL/desktop/hyprlandia.sh
-source $OMARCHY_INSTALL/desktop/theme.sh
-source $OMARCHY_INSTALL/desktop/bluetooth.sh
-source $OMARCHY_INSTALL/desktop/asdcontrol.sh
-source $OMARCHY_INSTALL/desktop/fonts.sh
-source $OMARCHY_INSTALL/desktop/printer.sh
+#show_logo slice 60
+#show_subtext "Installing desktop tools [3/5]"
+#source $OMARCHY_INSTALL/desktop/desktop.sh
+#source $OMARCHY_INSTALL/desktop/hyprlandia.sh
+#source $OMARCHY_INSTALL/desktop/theme.sh
+#source $OMARCHY_INSTALL/desktop/bluetooth.sh
+#source $OMARCHY_INSTALL/desktop/asdcontrol.sh
+#source $OMARCHY_INSTALL/desktop/fonts.sh
+#source $OMARCHY_INSTALL/desktop/printer.sh
 
 # Apps
-show_logo expand
-show_subtext "Installing default applications [4/5]"
-source $OMARCHY_INSTALL/apps/webapps.sh
-source $OMARCHY_INSTALL/apps/xtras.sh
-source $OMARCHY_INSTALL/apps/mimetypes.sh
+#show_logo expand
+#show_subtext "Installing default applications [4/5]"
+#source $OMARCHY_INSTALL/apps/webapps.sh
+#source $OMARCHY_INSTALL/apps/xtras.sh
+#source $OMARCHY_INSTALL/apps/mimetypes.sh
 
 # Updates
-show_logo highlight
-show_subtext "Updating system packages [5/5]"
-sudo updatedb
-yay -Syu --noconfirm --ignore uwsm
+#show_logo highlight
+#show_subtext "Updating system packages [5/5]"
+#sudo updatedb
+#yay -Syu --noconfirm --ignore uwsm
 
 # Reboot
-show_logo laseretch 920
-show_subtext "You're done! So we'll be rebooting now..."
-sleep 2
-reboot
+#show_logo laseretch 920
+#show_subtext "You're done! So we'll be rebooting now..."
+#sleep 2
+#reboot
